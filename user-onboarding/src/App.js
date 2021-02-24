01ee1c1f5cf37c1b7b564612ea679fc17e3f8f5f
Jonathan Ryan <jryan3328@gmail.com>
2021-02-24 16:27:54 -0500

    doneish

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,8 +1,32 @@
 import Form from './components/Form'
+import styled from 'styled-components'
+
+const StyledDiv = styled.div`
+  display: flex;
+  flex-flow: row-wrap;
+  justify-content: center;
+  align-items: start;
+
+`
+const Styledh1 = styled.h1`
+  font-size: 3rem;
+  font-family:arial;
+  color: red;
+  text-shadow: 2px 2px black;
+  text-align:right;
+  -webkit-text-stroke-width: 2px;
+  -webkit-text-stroke-color: silver;
+
+`
+
 
 function App() {
   return (
-    null
+    <div className='container'>
+    <Styledh1>The Homies</Styledh1>
+  
+    <Form/>
+    </div>
   )
 }
 
--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,10 +1,38 @@
 import React from 'react'
+import styled from 'styled-components'
+
+
+const StyledH3 = styled.h3`
+    font-size:2rem;
+    color: blue;
+
+`
+
+const StyledDiv = styled.div`
+    display:flex;
+    
+`
 
 function Form() {
     return (
-        <div>
-            
-        </div>
+        <StyledDiv>
+            <form className='form-container'>
+                <StyledH3>Sign Up</StyledH3>
+                <label>Username:
+                    <input type='text' name='username' placeholder='Create Your Username'/>
+                </label>
+                <label>Email:
+                    <input type='text' name='email' placeholder='Your Email'/>
+                </label>
+                <label>Password:
+                    <input type='text' name='password' placeholder='Enter a Password'/>
+                </label>
+                
+                
+
+            </form>
+            </StyledDiv>
+        
     )
 }
 
